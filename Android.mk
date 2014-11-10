32bbbad9355e41b96ab7f095b854b65c5f2511d8
Ethan Chen <intervigil@gmail.com>
2014-11-10 13:44:55 -0800

    audio: Only allow non-legacy HAL
    Change-Id: Icbb7874ebbc3b8edbe4bb483c32d4d106450293f
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,9 +2,7 @@ ifeq (msm8916,$(QCOM_HARDWARE_VARIANT))
 
 MY_LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-include $(MY_LOCAL_PATH)/legacy/Android.mk
-else
+ifneq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 ifneq ($(filter mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(MY_LOCAL_PATH)/hal_mpq/Android.mk
 else
