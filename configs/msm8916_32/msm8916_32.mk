47038c6d82b0498d6b3058c12db4a6e774d5c93b
Louis Popi <theh2o64@gmail.com>
2017-04-08 08:11:25 +0200

    audio: Use per-chipset path format
    Change-Id: Ic7238d39e86abc60bed14551778a57f392eeb230

--- configs/msm8916_32/msm8916_32.mk
+++ configs/msm8916_32/msm8916_32.mk
@@ -46,43 +46,43 @@ TARGET_USES_QCOM_MM_AUDIO := true
 ##AUDIO_FEATURE_FLAGS
 
 #Audio Specific device overlays
-DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
+DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/common/overlay
 
 USE_XML_AUDIO_POLICY_CONF := 1
 
 # Audio configuration file
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/common/media/audio_policy.conf:system/etc/audio_policy.conf
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/common/media/audio_policy.conf:system/etc/audio_policy.conf
 else
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf
 endif
 # Audio configuration file
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf \
-    hardware/qcom/audio/configs/msm8916_32/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_sbc.xml:system/etc/mixer_paths_sbc.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_skul.xml:system/etc/mixer_paths_skul.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8916_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    hardware/qcom/audio/configs/msm8916_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-    hardware/qcom/audio/configs/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    hardware/qcom/audio/configs/msm8916_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_sbc.xml:system/etc/mixer_paths_sbc.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_skul.xml:system/etc/mixer_paths_skul.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
 else
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio/configs/msm8916_32/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(TOPDIR)hardware/qcom/audio/$(QCOM_HARDWARE_VARIANT)/configs/msm8916_32/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
